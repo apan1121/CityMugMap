22e2029d7c63a182e6ab3d4c218b49b70666fae9
byron <byron@pressplay.cc>
2026-03-19 15:36:15 +0800

    [chg] 更新專案版本資訊以反映最新變更
    - version.json：更新 WEB_VER 與 WEB_VER_TIME，以顯示當前版本與時間戳記
    - version.php：調整版本定義方式，改為使用變數以提高可讀性
    - .gitignore：刪除不再需要的忽略檔案，確保專案結構清晰

--- config/version.php
+++ config/version.php
@@ -1,5 +1,7 @@
 <?php
     
-        define('WEB_VER', "dev");
-        define('WEB_VER_TIME', time());
+        $ver = "90ff0e9";
+        define('WEB_VER', $ver);
+        $ver_time = "1773905644";
+        define('WEB_VER_TIME', $ver_time);
     
